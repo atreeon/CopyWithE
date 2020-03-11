3758f236a828598cdc4e65bb1c7cec2f39568c01
Atree <atreeonhill@gmail.com>
2020-03-11 13:12:02 +0000

    Allow generics

--- copy_with_e_generator/lib/src/createCopyWith.dart
+++ copy_with_e_generator/lib/src/createCopyWith.dart
@@ -6,11 +6,8 @@ String createCopyWith(
   List<ClassDef> types,
 ) {
   var sb = StringBuffer();
-  sb.writeln("//" + extType.name);
-  sb.writeln("//" + types.map((e) => e.name + "|" + e.baseTypes.toString()).toList().toString());
   var types2 = orderTypes(extType, types);
 
-  sb.writeln("//" + types2.map((e) => e.name).toList().toString());
   sb.writeln(getExtensionDef(extType.name) + "{");
   sb.writeln(getCopyWithSignature(extType.name, extType.fields, extType.generics) + "{");
 
@@ -19,8 +16,6 @@ String createCopyWith(
       .forEach((type) {
     sb.writeln("if (this is ${type.name}) {");
     sb.writeln("return ${type.name}(");
-//    sb.writeln("//extType:" + extType.fields.map((e) => "${e.name}|${e.type}").toList().toString());
-//    sb.writeln("//type:" + type.fields.map((e) => "${e.name}|${e.type}").toList().toString());
     sb.writeln(getConstructorLines(extType, type) + ",");
     sb.writeln(");}");
   });
@@ -30,3 +25,9 @@ String createCopyWith(
 
   return sb.toString();
 }
+
+//  sb.writeln("//" + extType.name);
+//  sb.writeln("//" + types.map((e) => e.name + "|" + e.baseTypes.toString()).toList().toString());
+//  sb.writeln("//" + types2.map((e) => e.name).toList().toString());
+//    sb.writeln("//extType:" + extType.fields.map((e) => "${e.name}|${e.type}").toList().toString());
+//    sb.writeln("//type:" + type.fields.map((e) => "${e.name}|${e.type}").toList().toString());
